d178fa8498c77a008733530e302f7c73f9848fbf
jibauch94 <jib090994@gmail.com>
2017-09-20 14:43:52 +0200

    commit 2

--- src/BufferedReaderExample.java
+++ src/BufferedReaderExample.java
@@ -1,2 +1,19 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class BufferedReaderExample {
+    public static void Main(String args[]) throws FileNotFoundException, IOException{
+        BufferedReader in = new BufferedReader(new FileReader("Tekst eksempel.txt"));
+        String line = in.readLine();
+        while(line != null){
+            System.out.println(line);
+
+            line = in.readLine();
+        }
+        if(in != null){
+            in.close();
+        }
+    }
 }
